fd89094903d7343a086bd98b4e234ab2f9a2617a
Khushal-Coding <ckhushal1342gmail.com>
2025-08-30 00:32:26 +0630

    Add new files

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
-db=SQLAlchemy(app)
+# db=SQLAlchemy(app)
 
-class Todo(db.Model):
-    sno=db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(200), nullable=False)
-    desc=db.Column(db.String(500), nullable=False)
-    # date_created=db.Column(db.DateTime, default=datetime.utcnow)
+# class Todo(db.Model):
+#     sno=db.Column(db.Integer, primary_key=True)
+#     title=db.Column(db.String(200), nullable=False)
+#     desc=db.Column(db.String(500), nullable=False)
+#     # date_created=db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return f"{self.sno} - {self.title}"
+#     def __repr__(self):
+#         return f"{self.sno} - {self.title}"
 
 @app.route('/')
 def hello_world():
